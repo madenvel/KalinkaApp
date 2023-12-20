fda30e1bb7c41f7f03c625913874dc1ad27e4272
MadEnvel <envelsavinds@gmail.com>
2023-12-20 00:37:05 +0000

    Add custom list tile

---
+++ lib/custom_list_tile.dart
@@ -0,0 +1,113 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rpi_music/data_provider.dart';
+import 'package:rpi_music/soundwave.dart';
+import 'custom_cache_manager.dart';
+import 'data_model.dart';
+
+class CustomListTile extends StatelessWidget {
+  final BrowseItem browseItem;
+  final int? index;
+  final Widget? trailing;
+  final GestureTapCallback? onTap;
+
+  const CustomListTile(
+      {super.key,
+      required this.browseItem,
+      this.index,
+      this.trailing,
+      this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: _buildLeadingIcon(context),
+        title: Text(browseItem.name ?? 'Unknown title'),
+        subtitle: browseItem.subname != null ? Text(browseItem.subname!) : null,
+        trailing: trailing,
+        onTap: onTap,
+        visualDensity: VisualDensity.comfortable);
+  }
+
+  IconData _getFallbackIcon() {
+    if (browseItem.url == null) {
+      return Icons.help;
+    }
+
+    final String browseItemType = browseItem.url!.split('/')[1];
+    switch (browseItemType) {
+      case 'artist':
+        return Icons.person;
+      case 'album':
+        return Icons.album;
+      case 'track':
+        return Icons.music_note;
+      case 'playlist':
+        return Icons.playlist_play;
+      default:
+        return Icons.help;
+    }
+  }
+
+  Widget _buildLeadingIcon(BuildContext context) {
+    PlayerState state = context.watch<PlayerStateProvider>().state;
+    String playedTrackId = state.currentTrack?.id ?? '';
+    String? currentId = browseItem.id;
+    bool isCurrent = playedTrackId == currentId;
+
+    return SizedBox(
+        width: 50,
+        height: 50,
+        child: Stack(children: [
+          Opacity(
+              opacity: isCurrent ? 0.3 : 1.0,
+              child: Center(
+                  child: index != null
+                      ? _buildLeadingNumber()
+                      : _buildItemImage(browseItem, _getFallbackIcon()))),
+          isCurrent
+              ? const Center(child: SoundwaveWidget())
+              : const SizedBox.shrink()
+        ]));
+  }
+
+  Widget _buildLeadingNumber() {
+    return SizedBox(
+      width: 50,
+      height: 50,
+      child: Center(
+          child: Text("${index! + 1}",
+              style: const TextStyle(fontSize: 20.0, color: Colors.grey))),
+    );
+  }
+
+  Widget _buildItemImage(BrowseItem item, IconData fallbackIcon) {
+    String? image;
+    if (item.image != null) {
+      image = item.image!.small ?? item.image!.thumbnail;
+    }
+    bool rounded = false;
+    if (browseItem.url!.startsWith('/artist')) {
+      rounded = true;
+    }
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(rounded ? 25.0 : 0),
+        child: Container(
+            width: 50,
+            height: 50,
+            color: Colors.grey,
+            child: image == null
+                ? Icon(fallbackIcon, size: 50.0)
+                : Center(
+                    child: CachedNetworkImage(
+                    cacheManager: RpiMusicCacheManager.instance,
+                    imageUrl: image,
+                    placeholder: (context, url) =>
+                        Icon(fallbackIcon, size: 50.0),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error, size: 50.0),
+                  ))));
+  }
+}
