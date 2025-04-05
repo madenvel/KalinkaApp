df3ad42ec684c2f6f7bc02be36f983e991c32bfc
Dmitry Savin <envelsavinds@gmail.com>
2025-04-06 00:05:02 +0100

    Update colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 class KalinkaColors {
-  static const playButtonColor = Color.fromRGBO(109, 7, 29, 1);
+  static const primaryButtonColor = Color.fromRGBO(109, 7, 29, 1);
+  static const secondaryButtonColor = Color.fromRGBO(150, 150, 150, 1);
+  static const buttonTextColor = Color.fromRGBO(220, 220, 220, 1);
   static const progressBarColor = Color.fromRGBO(109, 7, 29, 1);
 
   static const inactiveProgressBarColor = Color.fromRGBO(200, 150, 150, 1);
